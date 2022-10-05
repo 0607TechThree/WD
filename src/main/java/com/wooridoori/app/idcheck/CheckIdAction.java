c136e2dce618786d85f5540e35ac80cd4ff3204f
dakakiyoo <ehdrb2531@gmail.com>
2022-10-05 16:20:11 +0900

    join

---
+++ src/main/java/com/wooridoori/app/idcheck/CheckIdAction.java
@@ -0,0 +1,51 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wooridoori.app.model.member.WdmemberDAO;
+import com.wooridoori.app.model.member.WdmemberVO;
+
+/**
+ * Servlet implementation class Check
+ */
+@WebServlet("/checkId.do")
+public class CheckIdAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckIdAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("로그1 ["+request.getParameter("userid")+"]");
+		
+		WdmemberDAO dao = new WdmemberDAO();
+		WdmemberVO vo = new WdmemberVO();
+		vo.setWdmid(request.getParameter("userid"));
+		int result = dao.checkId(vo);
+		
+		response.setContentType("application/x-json; charset=UTF-8");
+		response.getWriter().write(result+"");
+	}
+
+}
