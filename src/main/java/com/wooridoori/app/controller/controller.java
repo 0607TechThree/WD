bff4d8024382e166882d938bdad0c098879cd86a
Kchiho <chiho1998@naver.com>
2022-10-07 18:24:13 +0900

    이미지 저장 완 / 파일 이름 정하지 않음 현재 1로 하드코딩

--- src/main/java/com/wooridoori/app/controller/controller.java
+++ src/main/java/com/wooridoori/app/controller/controller.java
@@ -1,14 +1,18 @@
 package com.wooridoori.app.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.apache.commons.codec.binary.Base64;
 import com.wooridoori.app.board.WdboardVO;
 
 @Controller
@@ -23,7 +27,16 @@ public class controller {
 	@ResponseBody
 	public String boardinsert(WdboardVO wdbvo,HttpServletRequest request) throws IllegalStateException, IOException {
 		System.out.println("로그: 오냐?");
-		System.out.println(request.getParameter("fileData1"));
+		String base64 = request.getParameter("fileData1");
+		String imageDataBytes = base64.split(",")[1];
+		System.out.println(imageDataBytes);
+		
+		byte[] imageBytes = Base64.decodeBase64(imageDataBytes);
+		BufferedImage bufImg = ImageIO.read(new ByteArrayInputStream(imageBytes));
+
+		System.out.println(bufImg);
+		ImageIO.write(bufImg, "jpg", new File("D:\\0607KIM\\workspace\\WD\\src\\main\\webapp\\img\\ck\\1.jpg"));
+
 		//System.out.println(wdbvo);
 //		MultipartFile fileData=wdbvo.getFileData();
 //	      if(!fileData.isEmpty()) {
