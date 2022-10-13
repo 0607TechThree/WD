f889dc2f8c0b7dbafb786c7de946ad63c6a8b9f3
Kchiho <chiho1998@naver.com>
2022-10-13 15:30:05 +0900

    로그인 회원가입 기능(로케이션사용)

--- src/main/java/com/wooridoori/app/controller/controller.java
+++ src/main/java/com/wooridoori/app/controller/controller.java
@@ -96,14 +96,15 @@ public class controller {
 	}
 	
 	@RequestMapping("/login.do")
-    public String selectOneMember(WdmemberVO wdmvo, HttpSession session,Model model) {
+    public String selectOneMember(WdmemberVO wdmvo,HttpServletRequest request,HttpSession session,Model model) {
+		String paramLocation=request.getParameter("location");
 		wdmvo=wdmemberService.selectOneWdmember(wdmvo);
 		if(wdmvo==null) {
 			return "redirect:login.jsp";
 		}
 		else {
 			session.setAttribute("udata", wdmvo);
-			return "redirect:main.do";
+			return paramLocation;
 		}
     }
 
@@ -111,5 +112,17 @@ public class controller {
     public String logout(HttpSession session) {
         session.invalidate();
         return "redirect:main.do";
+    }
+    
+    @RequestMapping("/loginaction.do")
+    public String loginaction(HttpServletRequest request,HttpSession session) {
+    	String uri = request.getHeader("REFERER"); // 로그인을 누를 시 frontcontroller를 거치는데, 이 때 거치기 전 페이지의 주소를 저장
+		System.out.println("uri : "+uri);
+		String location = uri.substring(26); // uri에서 필요한 action만 자름
+		System.out.println(location);
+		
+		session.setAttribute("location", location);
+
+    	return "login.jsp";
     }
 }
