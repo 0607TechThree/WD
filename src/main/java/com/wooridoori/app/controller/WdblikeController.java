b60cf09be1f961063258d6584348f24014170219
Kchiho <kim@chihoui-MacBookAir.local>
2022-10-14 16:09:24 +0900

    like 로직

--- src/main/java/com/wooridoori/app/controller/WdblikeController.java
+++ src/main/java/com/wooridoori/app/controller/WdblikeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.wooridoori.app.board.WdboardService;
+import com.wooridoori.app.board.WdboardVO;
 import com.wooridoori.app.like.WdblikeService;
 import com.wooridoori.app.like.WdblikeVO;
 
@@ -12,16 +14,32 @@ public class WdblikeController {
 	
 	@Autowired
 	private WdblikeService WdblikeService;
+	@Autowired
+	private WdboardService WdboardService;
 	
-	@RequestMapping("/insert.do")
-	public String insertWdlike(WdblikeVO vo) {
+	@RequestMapping("/insertwdlike.do")
+	public String insertWdlike(WdblikeVO vo,WdboardVO wdbvo) {
 		WdblikeService.insertWdlike(vo);
+		wdbvo.setWdbpk(vo.getWdbpk());
+		WdboardService.updateWdboardlikeU(wdbvo);
 		return "redirect:main.do";
 	}
 	
-	@RequestMapping("/update.do")
-	public String updateWdlike(WdblikeVO vo) {
+	@RequestMapping("/selectOnewdlike.do")
+	public String selectOne(WdblikeVO vo) {
+		WdblikeService.selectOneWdlike(vo);
+		return "redirect:main.do";
+	}
+	
+	@RequestMapping("/updatewdlike.do")
+	public String updateWdlike(WdblikeVO vo,WdboardVO wdbvo) {
 		WdblikeService.updateWdlike(vo);
+		wdbvo.setWdbpk(vo.getWdbpk());
+		if(vo.getWdbpk() == 0) {
+			WdboardService.updateWdboardlikeD(wdbvo);
+		}else {
+			WdboardService.updateWdboardlikeU(wdbvo);
+		}
 		return "redirect:main.do";
 	}
 	
